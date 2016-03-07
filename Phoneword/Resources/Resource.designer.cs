3f2657545b69e912bf09bd20154381a4825b2b24
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2016-03-07 14:10:55 +0300

    Add multiscreen capability

--- Phoneword/Resources/Resource.designer.cs
+++ Phoneword/Resources/Resource.designer.cs
@@ -47,6 +47,9 @@ namespace Phoneword
 			// aapt resource value: 0x7f050003
 			public const int CallButton = 2131034115;
 			
+			// aapt resource value: 0x7f050004
+			public const int CallHistoryButton = 2131034116;
+			
 			// aapt resource value: 0x7f050001
 			public const int PhoneNumberText = 2131034113;
 			
@@ -103,6 +106,9 @@ namespace Phoneword
 			
 			// aapt resource value: 0x7f040000
 			public const int app_name = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int callHistory = 2130968577;
 			
 			static String()
 			{
--- Phoneword/Resources/layout/MainActivity.cs
+++ Phoneword/Resources/layout/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -11,6 +12,8 @@ namespace Phoneword
 	[Activity (Label = "Phone Word", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
+		static readonly List<string> phoneNumbers = new List<string>();
+
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
@@ -22,6 +25,7 @@ namespace Phoneword
 			EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
 			Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
 			Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+			Button callHistoryButton = FindViewById<Button> (Resource.Id.CallHistoryButton);
 			
 			// Disable the "Call" button
 			callButton.Enabled = false;
@@ -52,6 +56,10 @@ namespace Phoneword
 				var callDialog = new AlertDialog.Builder(this);
 				callDialog.SetMessage("Call " + translatedNumber + "?");
 				callDialog.SetNeutralButton("Call", delegate {
+					// add dialed number to list of called numbers.
+					phoneNumbers.Add(translatedNumber);
+					// enable the Call History button
+					callHistoryButton.Enabled = true;
 					// Create intent to dial phone
 					var callIntent = new Intent(Intent.ActionCall);
 					callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
@@ -61,6 +69,13 @@ namespace Phoneword
 
 				// Show the alert dialog to the user and wait for response.
 				callDialog.Show();
+			};
+
+			callHistoryButton.Click += (sender, e) =>
+			{
+				var intent = new Intent(this, typeof(CallHistoryActivity));
+				intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+				StartActivity(intent);
 			};
 		}
 	}
